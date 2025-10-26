6f164012c483ace718791f6bc6ec5c8bf5d34011
Bernabe <20221130@uthh.edu.mx>
2025-10-26 16:53:07 -0600

    Refactor mock Axios implementation and clean up download files
    - Simplified the mock Axios structure by removing unnecessary methods.
    - Deleted obsolete `.crdownload` files from the downloads directory to maintain cleanliness.

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -1,10 +1,4 @@
-// __mocks__/axios.js
-const mockAxios = {
-  get: jest.fn(() => Promise.resolve({ data: {} })),
-  post: jest.fn(() => Promise.resolve({ data: {} })),
-  put: jest.fn(() => Promise.resolve({ data: {} })),
-  delete: jest.fn(() => Promise.resolve({ data: {} })),
-  create: jest.fn(() => mockAxios),
-};
-
-export default mockAxios;
+export default {
+    get: jest.fn(() => Promise.resolve({ data: {} })),
+    post: jest.fn(() => Promise.resolve({ data: {} })),
+  };
